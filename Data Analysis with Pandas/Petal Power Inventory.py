e9c7a329cca73ee88019d6efc177ebd96ed02680
JinchunChoi <JinchunChoi@users.noreply.github.com>
2020-03-30 15:32:32 -0400

    Update Petal Power Inventory.py

--- Data Analysis with Pandas/Petal Power Inventory.py
+++ Data Analysis with Pandas/Petal Power Inventory.py
@@ -1,1 +1,27 @@
+import codecademylib
+import pandas as pd
+inventory = pd.read_csv('inventory.csv')
 
+# print(inventory.head(10))
+
+staten_island = inventory[inventory.location == 'Staten Island']
+# print(staten_island)
+
+product_request = staten_island['product_description']
+# print(product_request)
+
+seed_request = inventory[(inventory.location == 'Brooklyn') & (inventory.product_type == 'seeds')]
+# print(seed_request)
+
+inventory['in_stock'] = inventory.apply(lambda row: True if row['quantity'] > 0 else False, axis=1)
+# print(dinventoryf.head(10))
+
+inventory['total_value'] = inventory.price * inventory.quantity
+# print(inventory.head(10))
+
+combine_lambda = lambda row: \
+    '{} - {}'.format(row.product_type,
+                     row.product_description)
+
+inventory['full_description'] = inventory.apply(combine_lambda, axis=1)
+print(inventory.head(10))
